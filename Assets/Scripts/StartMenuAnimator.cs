dd587a4b60312c49a2c4a9b857bbe3102e906ef8
AlexanderCrane <InnovateTheWorld@yahoo.com>
2021-01-31 15:44:02 -0600

    Level started, title screen

---
+++ Assets/Scripts/StartMenuAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Animator>().SetBool("SitandLook", true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
