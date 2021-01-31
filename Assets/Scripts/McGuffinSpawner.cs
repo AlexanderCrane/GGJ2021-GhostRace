29759f31b4b21d14e289612d228eda01aeb73ba4
AlexanderCrane <InnovateTheWorld@yahoo.com>
2021-01-31 01:29:46 -0600

    Minor changes

---
+++ Assets/Scripts/McGuffinSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class McGuffinSpawner : MonoBehaviour
+{
+    public GameObject[] SpawnPoints;
+    public GameObject McGuffin;
+    Vector3 chosenPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chosenPoint = SpawnPoints[Random.Range(0,SpawnPoints.Length)].transform.position;
+        McGuffin.transform.position = chosenPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
