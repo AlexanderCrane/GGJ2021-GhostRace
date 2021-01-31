a2551d26b72299e264150036a481037f4a4e2673
joshua <questofiranon@gmail.com>
2021-01-30 19:48:54 -0800

    :alembic: ghost movement in the x z planes

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GhostController : InputBehaviour {
+
+    public Camera GhostEyes;
+    public float moveSpeed = 1.0f;
+
+    private Rigidbody body;
+
+    // I hear physicist screaming internally at my vasiable naming
+    private Vector2 magnitude = Vector2.zero;
+
+    private Vector2 lookMagnitude = Vector2.zero;
+
+    float lookX = 0f;
+    float lookY = 0f;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        this.body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+                var x = lookX * 10 * Time.deltaTime;
+        var y = lookY * 10 * Time.deltaTime;
+
+        // I miss Brackeys already ;-;
+        xRotation -= lookY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        GhostEyes.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * lookX);
+    }
+
+    void FixedUpdate() {
+        var moveD = new Vector3(magnitude.x, transform.forward.y, magnitude.y) * moveSpeed;
+
+        // using force because I want floaty movement for the ghost
+        body.AddRelativeForce(moveD, ForceMode.Impulse);
+
+    }
+
+    protected override void Look(Vector2 looking) {
+        lookX = looking.x;
+        lookY = looking.y;
+    }
+
+    protected override void Move(Vector2 movement) {
+        magnitude = movement;
+    }
+}
---
+++ Assets/Scripts/InputBehaviour.cs
@@ -0,0 +1,118 @@
+
+using UnityEngine;
+
+
+public class InputBehaviour : MonoBehaviour
+{
+    public InputMaster controls;
+
+    void Awake() {
+        controls = new InputMaster();
+        controls.Player.Move.Enable();
+        controls.Player.Look.Enable();
+        controls.Player.WestButton.Enable();
+        controls.Player.EastButton.Enable();
+        controls.Player.NorthButton.Enable();
+        controls.Player.SouthButton.Enable();
+        controls.Player.LeftBumper.Enable();
+        controls.Player.RightBumper.Enable();
+        controls.Player.LeftTrigger.Enable();
+        controls.Player.RightTrigger.Enable();
+        controls.Player.Confirm.Enable();
+        controls.Player.Start.Enable();
+        controls.Player.Back.Enable();
+        controls.Player.LeftStick.Enable();
+        controls.Player.RightStick.Enable();
+        controls.Player.WestButton.performed += _ => WestButtonPressed();
+        controls.Player.EastButton.performed += _ => EastButtonPressed();
+        controls.Player.NorthButton.performed += _ => NorthButtonPressed();
+        controls.Player.SouthButton.performed += _ => SouthButtonPressed();
+        controls.Player.LeftBumper.performed += _ => LeftBumperPressed();
+        controls.Player.RightBumper.performed += _ => RightBumperPressed();
+        controls.Player.LeftTrigger.performed += _ => LeftTriggerPressed();
+        controls.Player.RightTrigger.performed += _ => RightTriggerPressed();
+        controls.Player.Confirm.performed += _ => SouthButtonPressed();
+        controls.Player.Start.performed += _ => StartPressed();
+        controls.Player.Back.performed += _ => EastButtonPressed();
+        controls.Player.LeftStick.performed += _ => LeftStickPressed();
+        controls.Player.RightStick.performed += _ => RightStickPressed();
+        controls.Player.Move.performed += ctx => Move(ctx.ReadValue<Vector2>());
+        controls.Player.Move.canceled += ctx => Move(Vector2.zero);
+        controls.Player.Look.performed += ctx => Look(ctx.ReadValue<Vector2>());
+        controls.Player.Look.canceled += ctx => Look(Vector2.zero);
+    }
+
+    protected virtual void WestButtonPressed()
+    {
+        Debug.Log("West button pressed");
+    }
+
+    protected virtual void EastButtonPressed()
+    {
+        Debug.Log("Back/East button pressed");
+    }
+
+    protected virtual void NorthButtonPressed()
+    {
+        Debug.Log("North button pressed");
+    }
+
+    protected virtual void SouthButtonPressed()
+    {
+        Debug.Log("Confirm/South button pressed");
+    }
+
+    protected virtual void LeftBumperPressed()
+    {
+        Debug.Log("Left Bumper pressed");
+    }
+
+    protected virtual void RightBumperPressed()
+    {
+        Debug.Log("Right Bumper pressed");
+    }
+
+    protected virtual void LeftTriggerPressed()
+    {
+        Debug.Log("Left Trigger pressed");
+    }
+
+    protected virtual void RightTriggerPressed()
+    {
+        Debug.Log("Right Trigger pressed");
+    }
+
+    protected virtual void StartPressed()
+    {
+        Debug.Log("Start pressed");
+    }
+
+    protected virtual void LeftStickPressed()
+    {
+        Debug.Log("Left Stick pressed");
+    }
+
+    protected virtual void RightStickPressed()
+    {
+        Debug.Log("Right Stick pressed");
+    }
+
+    protected virtual void Move(Vector2 movement)
+    {
+        Debug.Log(movement);
+    }
+
+    protected virtual void Look(Vector2 looking)
+    {
+        Debug.Log(looking);
+    /*
+        if(Mathf.Abs(looking.y) > Mathf.Abs(looking.x)){
+            lookAxisText.SetText("Looking forward/back");
+        }
+        
+        if(Mathf.Abs(looking.x) > Mathf.Abs(looking.y)){
+            lookAxisText.SetText("Looking left/right");
+        }
+    */
+    }
+}
