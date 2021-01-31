27b42481f5dc4b7bc310083026cecb04f0d3572e
John Ying <holylifealchemist@hotmail.com>
2021-01-30 20:28:41 -0600

    Resolves #1 Mirror Multiplayer Asset

---
+++ Assets/JohnTest/Scripts/PlayerNetworkCommands.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+/// <summary>
+/// A client can call a command (client -> server). The server executes the command,
+/// and likely you will want an rpc called from the command (server -> client). An RPC is called on ALL clients.
+/// </summary>
+public class PlayerNetworkCommands : NetworkBehaviour
+{
+    private Transform playerCamera;
+
+    private void Start() {
+        playerCamera = GetComponent<PlayerNetworkManager>().playerCamera.transform;
+    }
+
+    /// <summary>
+    /// Call this function from a player script, and it will execute on the server.
+    /// This is unsafe.
+    /// </summary>
+    [Command]
+    public void CmdInteractRaycast()
+    {
+        Vector3 forward = playerCamera.TransformDirection(Vector3.forward).normalized;
+        float distance = 3.0f;
+
+        RaycastHit hit;
+        if (Physics.Raycast(playerCamera.position, forward, out hit, distance))
+        {
+            if (hit.transform.tag == "FakeDoorTag")
+            {
+                // hit.transform.GetComponent<Door>().RpcOpenDoor);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Example Command call. Function name MUST start with Cmd.
+    /// There may be instances where you do not need to do any checks. Checks are purely for security reasons on the server side.
+    /// </summary>
+    [Command]
+    private void CmdExampleFunction()
+    {
+        // Do any necessary checks or additional required actions.
+        // NetworkServer.Spawn(this.gameObject);
+
+        // Do the Rpc function call.
+    }
+
+    /// <summary>
+    /// Example RPC (Remote Procedure Call) call. Function name MUST start with Rpc.
+    /// </summary>
+    /// <param name="someObject">List of Supported Data Types: https://mirror-networking.com/docs/Articles/Guides/DataTypes.html</param>
+    [ClientRpc]
+    private void RpcExampleFunction(GameObject someObject)
+    {
+        // Do the actions on the client side
+        // NetworkServer.Destroy(someObject);
+    }
+}
---
+++ Assets/JohnTest/Scripts/PlayerNetworkManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerNetworkManager : NetworkBehaviour
+{
+    [Tooltip("A list of gameobject body parts that will be set inactive to the local player.")]
+    public List<GameObject> playerBodyParts;
+
+    [Header("Enabled Local Player Objects")]
+    public GameObject playerCamera;
+
+    // Start is called before the first frame update
+    public override void OnStartLocalPlayer()
+    {
+        if (GetComponent<PlayerNetworkCommands>() == null)
+        {
+            Debug.LogError("Add PlayerNetworkCommands not attached with PlayerNetworkManager.");
+        }
+
+        base.OnStartLocalPlayer();
+        DontDestroyOnLoad(this.gameObject);
+        if (GetComponent<NetworkIdentity>().isLocalPlayer)
+        {
+            enableLocalPlayerInput();
+            disableLocalBodyParts();
+
+            enableCameraComponents();
+        }
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer) return;
+        
+        //call raycast or local update methods here...
+    }
+
+    private void enableLocalPlayerInput()
+    {
+        /*
+         * Enable character input controllers here, for example:
+         * GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+         */
+    }
+
+    private void disableLocalBodyParts()
+    {
+        playerBodyParts.ForEach(go => go.SetActive(false));
+    }
+
+    private void enableCameraComponents()
+    {
+        //enable camera components here such as audio listener
+        playerCamera.GetComponent<Camera>().enabled = true;
+        playerCamera.GetComponent<AudioListener>().enabled = true;
+    }
+}
