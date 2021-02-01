2349f41767444e19064465b1cb6852ad08a25e38
John Ying <holylifealchemist@hotmail.com>
2021-01-31 19:29:28 -0600

    new game network manager

---
+++ Assets/JohnTest/Scripts/GameNetworkManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GameNetworkManager : NetworkManager
+{
+    public GameObject human;
+    public GameObject ghost;
+
+    public void changePlayerToHuman()
+    {
+        playerPrefab = human;
+    }
+
+    public void changePlayerToGhost()
+    {
+        playerPrefab = ghost;
+    }
+}
